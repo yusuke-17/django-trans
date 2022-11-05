bf88ef4c6c42591a29ec28747663709dd5aeb289
yusuke-17 <yuyr1711@gmail.com>
2022-11-05 15:22:51 +0900

    “bootstrap導入”

--- transfile/forms.py
+++ transfile/forms.py
@@ -1,4 +1,8 @@
 from django import forms
 
 class TransFileForm(forms.Form):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['sentence'].widget.attrs['class'] = 'mt-5'
+
     sentence = forms.CharField(label='翻訳(日本語)', widget=forms.Textarea(), required=True)
