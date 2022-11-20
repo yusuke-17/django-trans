a21a7daf518f47ed5fd36b53605dfc5e4b92bc3b
yusuke-17 <yuyr1711@gmail.com>
2022-11-20 14:00:12 +0900

    ダウンロード機能

--- transfile/forms.py
+++ transfile/forms.py
@@ -1,14 +1,7 @@
 from django import forms
 from .models import FileUpload
 
-class TransFileForm(forms.Form):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['sentence'].widget.attrs['class'] = 'mt-5'
-
-    sentence = forms.CharField(label='翻訳(日本語)', widget=forms.Textarea(), required=True)
-    
-# class FileUpload(forms.ModelForm):
-#     class Meta:
-#         model = FileUpload
-#         fields = ('files',)
+class TransFileForm(forms.ModelForm):
+    class Meta:
+        model = FileUpload
+        fields = ('uploadedFile',)
--- transfile/views.py
+++ transfile/views.py
@@ -10,49 +10,38 @@ from .forms import TransFileForm
 from django.http import FileResponse
 
 def index(request):
-    file_name = ""
     result_file = ""
-    trans_result = ""
+    file_name = ""
 
     if request.method == "POST":
-        # form = TransFileForm(request.POST)
-        uploadedFile = request.FILES["uploadedFile"]
-        document = models.FileUpload(
-            uploadedFile = uploadedFile
-        )
-        document.save()
-
-        # ファイル取得
-        # file = models.FileUpload.objects.order_by("id").last()
-        # file_name = file.uploadedFile.url
-        # root, ext = os.path.splitext(file_name)
-        # result_file = root + '_EN' + ext
-        
-        # translator = deepl.Translator(settings.DEEPL_API_KEY)
-        # # 日本語に翻訳
-        # trans_result = translator.translate_document_from_filepath(
-        #     file_name,
-        #     result_file,
-        #     source_lang='JA',
-        #     target_lang='EN-US'
-        # )
-
-        # if form.is_valid():
-        #     translator = deepl.Translator(settings.DEEPL_API_KEY)
-        #     # 翻訳文を取得
-        #     sentence = form.cleaned_data['sentence']
-        #     # 日本語に翻訳
-        #     trans_result = translator.translate_text(sentence, source_lang='JA', target_lang='EN-US')
-    # else:
-    #     form = TransFileForm()
+        form = TransFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            # ファイル取得
+            file = models.FileUpload.objects.order_by("id").last()
+            file_name = settings.BASE_DIR + file.uploadedFile.url
+            root, ext = os.path.splitext(file_name)
+            result_file = root + '_EN' + ext
+            
+            if file.uploadedFile.url and result_file:
+                translator = deepl.Translator(settings.DEEPL_API_KEY)
+                # 日本語に翻訳
+                translator.translate_document_from_filepath(
+                    file_name,
+                    result_file,
+                    source_lang='JA',
+                    target_lang='EN-US'
+                )
+                # ファイルパスと名前取得
+                dirname, basename = os.path.split(result_file)
+    else:
+        form = TransFileForm()
     temp = loader.get_template('transfile/index.html')
     context = {
-        'trans_result': trans_result
+        'trans_result': result_file
     }
-    # context = {
-    #     'form': form,
-    #     'trans_result': trans_result
-    # }
+    if result_file:
+        return FileResponse(open(result_file, "rb"), as_attachment=True, filename=basename)
     return HttpResponse(temp.render(context, request))
 
 # Create your views here.
